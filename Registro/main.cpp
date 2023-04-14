358c34f8fdb036c98da9e9ba011f9cb6b776c4ee
kate29and <katesanlon29@gmail.com>
2023-04-14 11:34:45 -0500

    Primera parte de registro de materias

--- Registro/main.cpp
+++ Registro/main.cpp
@@ -10,35 +10,82 @@ int main()
     int opc=0;
     int n=0;
     char ***Categ = nullptr;
-    char ***Categ2 = nullptr;
 
     cout<<"Ingresar numero de materias: ";
     cin>>n;
 
+    Categ = new char**[n+1]; // memoria para filas
+    for (int i=0; i<n+1 ; i++){
+        Categ[i] = new char*[5];//memoria para columnas
+        for (int j = 0; j < 5; j++) {
+            Categ[i][j] = new char[10]; // memoria para dato
+        }
+    }
     int s=0;
-    Categ = new char**[n];
-    for (int i=0; i<n; i++){
-        int a=6, b=8;
-        Categ[i] = new char*[5];
-        if(i==0){
-            for (int j=0;j<5;j++){
-                Categ[i][j] = new char[30];
-                cin>>Categ[i][j];
+
+    char m[] = "Horas-Materias-Codigo-Horario-Creditos-";
+    char* m_ptr = m;
+    for (int i = 0; i < n + 1; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (i == 0) {
+                for (int k = 0; *m_ptr != '-'; k++) {
+                    Categ[i][j][k] = *m_ptr; //apunta a m
+                    m_ptr++;
+                }
+                m_ptr++; // avanzar el puntero para saltar el espacio
+            }
+            else {
+                if (j == 0) {
+                    cout << "Ingrese horas de la materia " << i << ": ";
+                }
+                else if (j == 1) {
+                    cout << "Ingrese nombre de la materia " << i << ": ";
+                }
+                else if (j == 2) {
+                    cout << "Ingrese codigo de la materia " << i << ": ";
+                }
+                else if (j == 3) {
+                    cout << "Ingrese horas semanales de la materia " << i << ": ";
+                }
+                else if (j == 4) {
+                    cout << "Ingrese creditos de la materia " << i << ": ";
+                }
+                cin >> Categ[i][j];
+            }
+
+
+        }
+    }
+
+
+   /* for (int i = 0; i < n+1; i++) {
+        for (int j=0;j<5;j++){
+            if (i == 0) {
+                for (int k=0; m[s]!=' '; k++){
+                    Categ[i][j][k]=m[s];
+                    s++;
+                }
+            }
+            else {
+                Categ[i][j][0] = '*';
             }
         }
-        else{
-            Categ[i][j] = new char[30];
-            Categ[i][j]=a;
+    }*/
+
+
+    for (int i = 0; i < n+1; i++) {
+        for (int j = 0; j < 5; j++) {
+
+            cout << Categ[i][j] << ' ';
         }
+        cout << endl;
     }
 
-    s=0;
-    for (int i = 0; i < n; i++) {
-        for (int j=0;j<5;j++){
-            cout << Categ[i][j]<<'|';
-            s++;
+    for (int i = 0; i < n+1; i++) {
+        for (int j = 0; j < 5; j++) {
+            delete[] Categ[i][j];
         }
-        cout<<endl;
+        delete[] Categ[i];
     }
-
+    delete[] Categ;
 }
